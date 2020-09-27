5a6524454a59ed1946490dccd207593122f83826
wanggang <wjiwg.liu.gg@126.com>
2020-09-27 23:24:29 +0800

    update

--- commons/src/main/java/com/wanggang/springcloud/result/CommonResult.java
+++ commons/src/main/java/com/wanggang/springcloud/result/CommonResult.java
@@ -12,9 +12,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import com.alibaba.fastjson.JSONObject;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommonResult<T> {
 
     private int code;
