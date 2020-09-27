e9dcd2a464e18b4507953d47c60bcee089a536ba
wanggang <wjiwg.liu.gg@126.com>
2020-09-27 21:59:41 +0800

    update

--- User-1001/src/main/java/com/wanggang/springcloud/User1001Application.java
+++ User-1001/src/main/java/com/wanggang/springcloud/User1001Application.java
@@ -1,11 +1,9 @@
 package com.wanggang.springcloud;
 
-import com.wanggang.ribbon.RibbonRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @ClassName : UserApplication
@@ -17,7 +15,6 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-//@RibbonClient(name = "CLOUD-ORDER-SERVICE", configuration = RibbonRule.class)
 public class User1001Application {
     public static void main(String[] args) {
         SpringApplication.run(User1001Application.class,args);
